0b4d249c1f822a1c8111f1f1d79d9cedafda3064
Shifterok <91421090+Shifterok@users.noreply.github.com>
2021-11-01 10:35:28 +0300

    Update MainKtTest.kt
    add changes *

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,23 +1,19 @@
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-// Если честно вообще не понял что происходит и что не так... в лекции как то разбор идет
-//с большим количеством перменных Интов и тп., а у меня по сути метод Воидовый, который ничего не возвращает, и как для него
-//написать какой то тест вообще не понял, в инете инфы на русском очень мало, прям совсем, буду рал если подскажите как хоть
-//под эту программу какой то тест написать
 class MainKtTest {
 
     @Test
-    fun transferFounds_areVkPay() {
-        //arrange
-        val selectPaySystem = 5
-        val money = 10_000
+    fun transferFounds(whatPaySystem: Int, amo: Int, finalAmount: Double) {
+        val paySystem = 1
+        val amount = 15_000
+        val amountOfTransaction = 14_660.0
 
         val resoult = transferFounds(
-            whatPaySystem = selectPaySystem,
-            amount = money,
+            whatPaySystem = paySystem,
+            amo = amount,
+            finalAmount = amountOfTransaction
         )
-        //assert
-        assertEquals(10_000, resoult )
+        assertEquals(15000,resoult)
     }
 }
