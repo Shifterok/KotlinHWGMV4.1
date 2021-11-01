5ffcdbb20d2fdac77c8db025a938232e7a9b3731
Shifterok <Lokky123@mail.ru>
2021-11-01 19:57:50 +0300

    after refactoring

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,30 +8,28 @@ const val discountMaestro = 0.04
 const val discountVkPay = 0
 
 //Функция расчета комиссии transferFounds
-fun transferFounds(whatPaySystem: Int = 5, amount: Int= 10_000) {
+fun transferFounds(amount: Double = 12_750.00, whatPaySystem: Int = 2): Double {
     println("Какой картой (платежной системой) будем платить? выберите по номеру из списка:")
     println("1. Visa")
     println("2. МИР")
     println("3. Mastercard")
     println("4. Maestro")
     println("5. Платежной системой VK Pay")
-    val whatPaySystem = readLine()!!.toInt()
-    println("Какую сумму переводим?")
-    val amount = readLine()!!.toDouble()
+
     val finalPrice = when (whatPaySystem) {
         1 -> amount - (amount * discountVisa - 35.00)
         2 -> amount - (amount * discountMir - 35.00)
         3 -> amount - (amount * discountMastercard - 20.00)
         4 -> amount - (amount * discountMaestro - 20.00)
         5 -> amount - amount * discountVkPay
-        else -> println("Ошибка... не корректный ввод")
+        else -> error("Ошибка... не корректный ввод")
     }
-    val finalPriceAtDouble = finalPrice as Double
-    val amountOfTransaction = (finalPriceAtDouble * 100.0).roundToInt() / 100.0
+    val amountOfTransaction = (finalPrice * 100.0).roundToInt() / 100.0
     println("Вы успешно перевели: $amountOfTransaction рублей")
+    return amountOfTransaction
 }
 
-fun main(selectPaySystem: Int, money: Int) {
+fun main() {
     //Проверка лимиотов совершенных переводов
     println("Укажите обущю сумму совершенных Вами переводов за день:")
     val amountAtDay = readLine()!!.toDouble()
@@ -40,6 +38,6 @@ fun main(selectPaySystem: Int, money: Int) {
     if (amountAtDay >= 150_000.00 || amountAtMounth >= 600_000.0) {
         println("Лимиты переводов превышены, операция отменена")
     } else {
-        transferFounds(selectPaySystem, money)
+        transferFounds()
     }
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,23 +1,27 @@
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-// Если честно вообще не понял что происходит и что не так... в лекции как то разбор идет
-//с большим количеством перменных Интов и тп., а у меня по сути метод Воидовый, который ничего не возвращает, и как для него
-//написать какой то тест вообще не понял, в инете инфы на русском очень мало, прям совсем, буду рал если подскажите как хоть
-//под эту программу какой то тест написать
 class MainKtTest {
-
     @Test
-    fun transferFounds_areVkPay() {
-        //arrange
-        val selectPaySystem = 5
-        val money = 10_000
+    fun transferFounds_correctRoundToRuble() {
+        val amountOfTransaction = 15_000.00
+        val paySystem = 2
 
         val resoult = transferFounds(
-            whatPaySystem = selectPaySystem,
-            amount = money,
+            amount = amountOfTransaction,
+            whatPaySystem = paySystem,
         )
-        //assert
-        assertEquals(10_000, resoult )
+        assertEquals(14_660.01, resoult, 0.1)
+    }
+    @Test
+    fun transferFounds_correctRoundToPenny() {
+        val amountOfTransaction = 15_000.00
+        val paySystem = 2
+
+        val resoult = transferFounds(
+            amount = amountOfTransaction,
+            whatPaySystem = paySystem,
+        )
+        assertEquals(14_660.01, resoult, 0.01)
     }
 }
